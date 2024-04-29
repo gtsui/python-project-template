b41702cf15ce0c9a45125e9e050f6631202f4688
tsuigeo <tsui.geoffrey@gmail.com>
2024-04-29 12:54:16 +0800

    update notebook

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+class Value():
+    def __init__(self, data:int):
+        self.data = data
+
+    def __repr__(self):
+        return f"Value(data={self.data})"
+
 if __name__ == "__main__":
     print("Hello World!")
